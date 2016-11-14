6a2f26d3c19f1aab130ca2949818f7befdd6bde0
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-11-14 15:22:44 +0000

    create duplicate neo4j containing biosamples data

--- populate-neo4j/load.sh
+++ populate-neo4j/load.sh
@@ -12,21 +12,20 @@ echo "Creating new database..."
 time nice "$NEO4J_BIN"/neo4j-import --bad-tolerance 10000 --into "$NEO4J_DATA/graph.db.tmp" --i-type string \
         --nodes:Sample "$NEO4J_DATA/samples.csv" \
         --nodes:Attribute "$NEO4J_DATA/attributes.csv" \
-        --relationships:hasAttribute "$NEO4J_DATA/has_attribute.csv"
-        
-#        --nodes:AttributeType "$NEO4J_DATA/types.csv" \
-#        --nodes:AttributeValue "$NEO4J_DATA/values.csv" \
-#        --nodes:OntologyTerm "$NEO4J_DATA/ontologies.csv" \
-#        --relationships:hasValue "$NEO4J_DATA/has_value.csv" \
-#        --relationships:hasType "$NEO4J_DATA/has_type.csv" \
-#        --relationships:hasIri "$NEO4J_DATA/has_iri.csv"
+        --nodes:AttributeType "$NEO4J_DATA/types.csv" \
+        --nodes:AttributeValue "$NEO4J_DATA/values.csv" \
+        --nodes:OntologyTerm "$NEO4J_DATA/ontologies.csv" \
+        --relationships:hasAttribute "$NEO4J_DATA/has_attribute.csv" \
+        --relationships:hasValue "$NEO4J_DATA/has_value.csv" \
+        --relationships:hasType "$NEO4J_DATA/has_type.csv" \
+        --relationships:hasIri "$NEO4J_DATA/has_iri.csv"
 
 # Create indexes
 echo "Creating indexes..."
 time nice "$NEO4J_BIN"/neo4j-shell -path "$NEO4J_DATA"/graph.db.tmp -file /indexes.cypher
 
 # Replace graph
-rm -rf "$NEO4J_DATA/graph.db"
-mv "$NEO4J_DATA/graph.db.tmp" "$NEO4J_DATA/graph.db"
+rm -rf "$NEO4J_DATA"/graph.db
+mv "$NEO4J_DATA"/graph.db.tmp "$NEO4J_DATA"/graph.db
 
 echo "All Done!"
