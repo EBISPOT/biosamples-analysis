1400c6b3ccf7cecf842ce0d0425b9d10d5908a6e
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-11-15 10:42:37 +0000

    remove threading for alpine container compatability

--- create-csv/create-csv.sh
+++ create-csv/create-csv.sh
@@ -12,16 +12,12 @@ cat /data/tmp_samples.csv | head -n 1 > /data/samples.csv
 cat /data/tmp_types.csv | head -n 1 > /data/types.csv
 cat /data/tmp_values.csv | head -n 1 > /data/values.csv
 
-cat /data/tmp_attributes.csv | tail -n +2 | sort -u >> /data/attributes.csv &
-cat /data/tmp_has_attribute.csv | tail -n +2 | sort -u >> /data/has_attribute.csv &
-cat /data/tmp_has_iri.csv | tail -n +2 | sort -u >> /data/has_iri.csv &
-cat /data/tmp_has_type.csv | tail -n +2 | sort -u >> /data/has_type.csv &
-cat /data/tmp_has_value.csv | tail -n +2 | sort -u >> /data/has_value.csv &
-cat /data/tmp_ontologies.csv | tail -n +2 | sort -u >> /data/ontologies.csv &
-cat /data/tmp_samples.csv | tail -n +2 | sort -u >> /data/samples.csv &
-cat /data/tmp_types.csv | tail -n +2 | sort -u >> /data/types.csv &
-cat /data/tmp_values.csv | tail -n +2 | sort -u >> /data/values.csv &
-
-wait
-
-rm -rf /data/tmp_*.csv
+cat /data/tmp_attributes.csv | tail -n +2 | sort -u >> /data/attributes.csv
+cat /data/tmp_has_attribute.csv | tail -n +2 | sort -u >> /data/has_attribute.csv
+cat /data/tmp_has_iri.csv | tail -n +2 | sort -u >> /data/has_iri.csv
+cat /data/tmp_has_type.csv | tail -n +2 | sort -u >> /data/has_type.csv
+cat /data/tmp_has_value.csv | tail -n +2 | sort -u >> /data/has_value.csv
+cat /data/tmp_ontologies.csv | tail -n +2 | sort -u >> /data/ontologies.csv
+cat /data/tmp_samples.csv | tail -n +2 | sort -u >> /data/samples.csv
+cat /data/tmp_types.csv | tail -n +2 | sort -u >> /data/types.csv
+cat /data/tmp_values.csv | tail -n +2 | sort -u >> /data/values.csv
