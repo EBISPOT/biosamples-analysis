5fc2b184028c551a1d2c61348c6e9e6293bfd672
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-11-16 17:32:53 +0000

    fixing type links and proportion iri generation

--- neo4j-analysis/neo4j-analysis.py
+++ neo4j-analysis/neo4j-analysis.py
@@ -18,11 +18,11 @@ def get_most_common_attributes(db_driver, n_attributes):
 	with db_driver.session() as session:
 		results = session.run("MATCH (s:Sample)-[:hasAttribute]->(:Attribute)-->(t:AttributeType) "
 		                      "WITH t, COUNT(s) AS usage_count "
-		                      "RETURN t.name as type, usage_count "
+		                      "RETURN t.attributeTypeId AS type, usage_count "
 		                      "ORDER BY usage_count DESC "
 		                      "LIMIT %d" % n_attributes)
-		for record in results:
-			attr_types.append(record['type'])
+		for result in results:
+			attr_types.append((result["type"],result["usage_count"]))
 	return attr_types
 
 
@@ -32,22 +32,21 @@ def generate_spreadsheet(db_driver):
 	with open("attr_common.csv", "w") as outfile:
 		csvout = csv.writer(outfile)
 
-		with db_driver.session() as session:
-			results = session.run("MATCH (s:Sample)-->(a:Attribute)-->(t:AttributeType) WITH a.type AS type, COUNT(s) AS usage_count RETURN type, usage_count ORDER BY usage_count DESC LIMIT 100")
-			for result in results:
-				print "{} ({})".format(result["type"], result["usage_count"])
+		common = get_most_common_attributes(db_driver, 100)
+		for attr in common:
+			print "{} ({})".format(attr[0], attr[1])
 
-				row = ["{} ({})".format(result["type"], result["usage_count"])]
+			row = ["{} ({})".format(attr[0], attr[1])]
 
-				with db_driver.session() as session2:
-					cypher = "MATCH (s:Sample)-->(a:Attribute{type:'" + str(result["type"]) + "'})-->(v:AttributeValue) WITH v,COUNT(s) AS usage_count RETURN v.attributeValueId AS value, usage_count ORDER BY usage_count DESC LIMIT 10"
-					print cypher
-					results2 = session2.run(cypher)
-					for result2 in results2:
-						row.append("{} ({})".format(result2["value"], result2["usage_count"]))
-						print "{} ({})".format(result2["value"], result2["usage_count"])
+			with db_driver.session() as session2:
+				cypher = "MATCH (s:Sample)-->(a:Attribute)-->(t:AttributeType{attributeTypeId:'"+attr[0]+"'}), (a:Attribute)-->(v:AttributeValue) RETURN v.attributeValueId AS value, COUNT(s) AS usage_count ORDER BY usage_count DESC LIMIT 10"
+				print cypher
+				results2 = session2.run(cypher)
+				for result2 in results2:
+					row.append("{} ({})".format(result2["value"], result2["usage_count"]))
+					print "{} ({})".format(result2["value"], result2["usage_count"])
 
-				csvout.writerow(row)
+			csvout.writerow(row)
 
 	print "generated spreadsheet of most common attribute types and values"
 
@@ -56,30 +55,33 @@ def generate_wordcloud(db_driver):
 
 	print "generating wordcloud of most common attribute types and values"
 	freq = []
-	with db_driver.session() as session:
-		results = session.run("MATCH (s:Sample)-->(a:Attribute) WITH a,COUNT(s) AS usage_count RETURN a.type, usage_count ORDER BY usage_count DESC LIMIT 1000")
+	
+	common = get_most_common_attributes(db_driver, 10)
+	for attr in common:
 		i = 0
-		for result in results:
-			freq.append((result["a.type"], result["usage_count"]))
-			if i < 25:
-				i += 1
-				print "generating wordcloud of values of", result["a.type"]
-				freq2 = []
-				with db_driver.session() as session2:
-					cypher = \
-						"MATCH (s:Sample)-->(a:Attribute{type:'" + str(result["a.type"]) + "'}) " \
-						"WITH a,COUNT(s) AS usage_count " \
-						"RETURN a.value AS value, usage_count " \
-						"ORDER BY usage_count DESC " \
-						"LIMIT 1000"
-					results2 = session2.run(cypher)
-					for result2 in results2:
-						freq2.append((result2["value"], result2["usage_count"]))
-				wc = wordcloud.WordCloud(width=640, height=512, scale=2.0, max_words=1000).generate_from_frequencies(freq2)
-				wc.recolor(color_func=grey_color_func, random_state=3)
-				wc.to_file("word_clouds/cloud-values-{}.png".format(result["a.type"]))
-				print "generated wordcloud of values of", result["a.type"]
-		wordcloud.WordCloud(width=640, height=512, scale=2.0, max_words=1000).generate_from_frequencies(freq).to_file("cloud-types.png")
+			
+		freq.append((attr[0], attr[1]))
+		if i < 25:
+			i += 1
+			print "generating wordcloud of values of", attr[0]
+			freq2 = []
+			with db_driver.session() as session2:
+				cypher = \
+					"MATCH (s:Sample)-->(a:Attribute{type:'" + str(attr[0]) + "'}) " \
+					"WITH a,COUNT(s) AS usage_count " \
+					"RETURN a.value AS value, usage_count " \
+					"ORDER BY usage_count DESC " \
+					"LIMIT 1000"
+				results2 = session2.run(cypher)
+				for result2 in results2:
+					freq2.append((result2["value"], result2["usage_count"]))
+			wc = wordcloud.WordCloud(width=640, height=512, scale=2.0, max_words=1000).generate_from_frequencies(freq2)
+			wc.recolor(color_func=grey_color_func, random_state=3)
+			wc.to_file("word_clouds/cloud-values-{}.png".format(attr[0]))
+			print "generated wordcloud of values of", attr[0]
+		wc = wordcloud.WordCloud(width=640, height=512, scale=2.0, max_words=1000).generate_from_frequencies(freq)
+		wc.recolor(color_func=grey_color_func, random_state=3)
+		wc.to_file("word_clouds/cloud-types.png")		
 	print "generated wordcloud of most common attribute types and values"
 
 
@@ -91,12 +93,13 @@ def attribute_values_mapped(db_driver):
 
 		common_attrs = get_most_common_attributes(db_driver, 100)
 		for attr in common_attrs:
-			cypher = "MATCH (s:Sample)-->(a:Attribute{type: \"%s\"}) " \
+			cypher = "MATCH (s:Sample)-->(a:Attribute{type:'"+attr[0]+"'}) " \
 					 "RETURN COUNT(s) AS samples, " \
-					 "COUNT(a.iri) AS mapped " % attr
+					 "COUNT(a.iri) AS mapped "
 			with db_driver.session() as session:
 				result = session.run(cypher)
 				for record in result:
+					print attr, record["samples"], record["mapped"]
 					row = [str(attr), round(float(record["mapped"])*100/record["samples"], 2)]
 					print row
 					csvout.writerow(row)
