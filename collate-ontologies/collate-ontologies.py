e037c1abef309bce2e3f493a32801b25abb00f74
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-12-02 17:14:47 +0000

    performance tweaks

--- collate-ontologies/collate-ontologies.py
+++ collate-ontologies/collate-ontologies.py
@@ -1,10 +1,13 @@
 # pip install requests unicodecsv
 import os
 import sys
-import requests
 import argparse
-import unicodecsv
 import json
+
+import requests
+import unicodecsv
+
+from requests_futures.sessions import FuturesSession
 
 
 class OntologyTerm:
@@ -16,32 +19,11 @@ class OntologyTerm:
         self.parents = parents
 
 
-def write_results(results, ontology):
-    term_file = "data/tmp_{}_terms.csv".format(ontology)
-    parent_file = "data/tmp_{}_parents.csv".format(ontology)
-
-    term_file = os.path.abspath(term_file)
-    parent_file = os.path.abspath(parent_file)
-    if not os.path.exists(os.path.dirname(term_file)):
-        os.makedirs(os.path.dirname(term_file))
-
-    if not os.path.exists(term_file):
-        with open(term_file, 'w') as f:
-            term_writer = unicodecsv.writer(f, delimiter=",")
-            term_writer.writerow(["iri:ID(EfoOntologyTerm)", "label", "synonyms[]", "parents[]", "obsolete"])
-    if not os.path.exists(parent_file):
-        with open(parent_file, 'w') as f:
-            parent_writer = unicodecsv.writer(f, delimiter=",")
-            parent_writer.writerow([":START_ID(EfoOntologyTerm)", ":END_ID(EfoOntologyTerm)"])
-
-    with open(term_file, 'a') as f:
-        with open(parent_file, 'a') as p:
-            term_writer = unicodecsv.writer(f, delimiter=",")
-            parent_writer = unicodecsv.writer(p, delimiter=",")
-            for result in results:
-                term_writer.writerow([result.iri, result.label, ";".join(result.synonyms), ";".join(result.parents), result.is_obsolete])
-                for parent in result.parents:
-                    parent_writer.writerow([result.iri, parent])
+def write_results(results, ontology, term_writer, parent_writer):
+    for result in results:
+        term_writer.writerow([result.iri, result.label, ";".join(result.synonyms), ";".join(result.parents), result.is_obsolete])
+        for parent in result.parents:
+            parent_writer.writerow([result.iri, parent])
 
 
 def get_parents(term_content):
@@ -51,13 +33,12 @@ def get_parents(term_content):
         pres = requests.get(parent_url)
 
         if pres.status_code == 200:
-            content = json.loads(pres.content)
-            n_page = 0
+            content = pres.json()
             total_pages = content["page"]["totalPages"]
             # n_parent = 0
             # total_parents = content["page"]["totalElements"]
 
-            while n_page < total_pages:
+            for n_page in xrange(0,total_pages):
                 parent_page_url = "{}?page={:d}".format(parent_url, n_page)
                 pres = requests.get(parent_page_url)
                 if pres.status_code == 200:
@@ -72,6 +53,9 @@ def get_parents(term_content):
 def get_url(kargs):
     return "http://{hostname}/api/ontologies/{ontology}/terms?page={page}&size={size}".format(**kargs)
 
+def bg_cb(sess, resp):
+    # parse the json storing the result on the response object
+    resp.data = resp.json()
 
 def main(argv):
     parser = argparse.ArgumentParser()
@@ -83,22 +67,58 @@ def main(argv):
 
     nav_options = vars(args)
     check_url = get_url(nav_options)
-
+    
+    #get an intial URL to get total pages
     initial_response = requests.get(check_url)
-    if initial_response.status_code == 200:
-        content = json.loads(initial_response.content)
-        total_pages = content["page"]["totalPages"]
-        total_elements = content["page"]["totalElements"]
-        n_element = (args.page * args.size)
-        print "Total number of elements {:d}; Starting from element {:d}".format(total_elements, n_element)
+    if initial_response.status_code != 200:
+        print "Problem getting initial page"
+        return
+    content = json.loads(initial_response.content)
+    total_pages = content["page"]["totalPages"]
+    total_elements = content["page"]["totalElements"]
+    n_element = (args.page * args.size)
+    print "Total number of elements {:d}; Starting from element {:d}".format(total_elements, n_element)
+        
+    #now we can create the writers to handle output
+    term_file = "data/tmp_{}_terms.csv".format(args.ontology)
+    term_file = os.path.abspath(term_file)
+    if not os.path.exists(os.path.dirname(term_file)):
+        os.makedirs(os.path.dirname(term_file))
+    
+    parent_file = "data/tmp_{}_parents.csv".format(args.ontology)
+    parent_file = os.path.abspath(parent_file)
+    if not os.path.exists(os.path.dirname(parent_file)):
+        os.makedirs(os.path.dirname(parent_file))
 
-        while nav_options["page"] < total_pages:
-            page_url = get_url(nav_options)
+    #open the writers
+    term_fieldnames = ["iri:ID(EfoOntologyTerm)", "label", "synonyms[]", "parents[]", "obsolete"]
+    parent_fieldnames = [":START_ID(EfoOntologyTerm)", ":END_ID(EfoOntologyTerm)"]
+    with open(term_file, 'w') as term_writer:
+        term_writer = unicodecsv.writer(term_writer, delimiter=",")
+        term_writer.writerow(term_fieldnames)
+        with open(parent_file, 'w') as parent_writer:
+            parent_writer = unicodecsv.writer(parent_writer, delimiter=",")
+            parent_writer.writerow(parent_fieldnames)
+        
+            #prepare for asynchronisity
+            session = FuturesSession(max_workers=25)
+            futures = []
+        
+            #now get each page and parse it
+            for page_i in xrange(args.page, total_pages):
+                nav_options["page"] = page_i
+                page_url = get_url(nav_options)
 
-            res = requests.get(page_url)
-            content = json.loads(res.content)
-            if res.status_code == 200:
+                futures.append(session.get(page_url, background_callback=bg_cb))
+                
+            for future in futures:
+                res = future.result()
+                content = res.data
+                if res.status_code != 200:
+                    print "Problem getting", page_url
+                    return
 
+                #loop over each term on the page
                 terms = content["_embedded"]["terms"]
                 results = []
                 for term in terms:
@@ -108,8 +128,10 @@ def main(argv):
                         synonyms = [s.strip().encode("utf-8") for s in term["synonyms"]]
                     else:
                         synonyms = []
+
                     label = term["label"].encode("utf-8").strip()
                     is_obsolete = term["is_obsolete"]
+                    #call out to get all the parent term pages too
                     term_parents = get_parents(term)
 
                     ontology_term = OntologyTerm(iri=iri, synonyms=synonyms, label=label,
@@ -121,12 +143,7 @@ def main(argv):
                                                                           n_element,
                                                                           total_elements)
 
-                write_results(results, nav_options["ontology"])
-            nav_options["page"] += 1
-        print "Page {:d}/{:d} - Element {:d}/{:d}".format(nav_options["page"],
-                                                          total_pages,
-                                                          n_element-1,
-                                                          total_elements)
+                    write_results(results, nav_options["ontology"], term_writer, parent_writer)
 
     print "\nProcess finished"
 
--- collate-ontologies/entrypoint.sh
+++ collate-ontologies/entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 set -e
 
-python -u collate-ontologies.py -o ncbitaxon -s 10000
+
+python -u collate-ontologies.py -o ncbitaxon -s 10000 &
+python -u collate-ontologies.py -o efo &
+wait
+
+cat /data/tmp_ncbitaxon_terms.csv | head -n 1 > /data/ncbitaxon_terms.csv
+cat /data/tmp_ncbitaxon_terms.csv | tail -n +2 | sort -u >> /data/ncbitaxon_terms.csv
+
+cat /data/tmp_ncbitaxon_parents.csv | head -n 1 > /data/ncbitaxon_parents.csv
+cat /data/tmp_ncbitaxon_parents.csv | tail -n +2 | sort -u >> /data/ncbitaxon_parents.csv
+
 
 
 cat /data/tmp_efo_terms.csv | head -n 1 > /data/efo_terms.csv
@@ -9,5 +19,7 @@ cat /data/tmp_efo_terms.csv | tail -n +2 | sort -u >> /data/efo_terms.csv
 
 cat /data/tmp_efo_parents.csv | head -n 1 > /data/efo_parents.csv
 cat /data/tmp_efo_parents.csv | tail -n +2 | sort -u >> /data/efo_parents.csv
+
+
 
 
